85d572d3aaafdfe9a69ac93a44a7c36751195348
Mayukh-Sen24 <mayukhsen049@gmail.com>
2026-05-27 21:49:45 +0530

    subject card component

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,8 +1,12 @@
+from os import name
+
 import streamlit as st
 from src.components.footer import footer_dashboard
 from src.ui.base_layout import style_background_dashboard, style_base_layout
 from src.components.header import header_dashboard
-from src.database.db import check_teacher_exist, create_teacher,teacher_login
+from src.database.db import check_teacher_exist, create_teacher,teacher_login,get_teacher_subjects
+from src.components.dialog_create_subject import create_subject_dialog
+from src.components.subject_card import subject_card
 
 def teacher_screen():
     style_base_layout()
@@ -61,7 +65,33 @@ def teacher_dashboard():
 def teacher_tab_take_attendance():
     st.header('Take AI Attendance')
 def teacher_tab_manage_subjects():
-    st.header('Manage your subjects and students here')
+    teacher_id=st.session_state.teacher_data['teacher_id']
+    col1,col2=st.columns(2)
+    with col1:
+        st.header("Manage subjects here", width='stretch')
+    with col2:
+        if st.button('Create new subject',width='stretch'):
+            create_subject_dialog(teacher_id)
+
+
+    #LIST all SUBJECTS
+    subjects=get_teacher_subjects(teacher_id)
+    if subjects:
+        for sub in subjects:
+            stats = [
+                        ("👨‍🎓", "Students", sub['total_students']),
+                        ("📚", "Classes", sub['total_classes'])
+                ]
+        def share_btn():
+           if st.button(f"Share Code: {sub['name']}",key=f"share_{sub['subject_code']}",icon=':material/share:',width='stretch'):
+               share_subject_dialog(sub['name'],sub['subject_code'])
+           st.space()
+        
+        subject_card(name=sub['name'],code=sub['subject_code'],section=sub['section'],stats=stats,footer_callback=share_btn)           
+        
+    else:
+        st.warning("No subjects found. Please create a subject to start taking attendance and managing students.")
+              
 def teacher_tab_attendance_records():
     st.header('View attendance records here')        
 def login_teacher(username,password):
