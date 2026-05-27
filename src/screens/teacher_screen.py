66b2c129aefac1d0369df0c1076f11749fd6e4e4
Mayukh-Sen24 <mayukhsen049@gmail.com>
2026-05-27 23:44:37 +0530

    QR implementation

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -7,6 +7,8 @@ from src.components.header import header_dashboard
 from src.database.db import check_teacher_exist, create_teacher,teacher_login,get_teacher_subjects
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.subject_card import subject_card
+from src.components.dialog_share_subject import share_subject_dialog
+
 
 def teacher_screen():
     style_base_layout()
