f643fc18cd7fc23e3af5dcced0b9cf4634976ba2
Mayukh-Sen24 <mayukhsen049@gmail.com>
2026-05-29 02:50:27 +0530

    domain name change

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 @st.dialog("Share class Link")
 
 def share_subject_dialog(subject_name,subject_code):
-    app_domain="snapclass-main.streamlit.app"
+    app_domain="snapclass-mainms.streamlit.app"
     join_url=f"{app_domain}/?join_code={subject_code}"
 
 
